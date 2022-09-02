10d06e6fcdaf8710884994e6f17a012de1708b82
theandersonn <theandersonn@gmail.com>
2022-09-02 18:28:33 -0300

    chore: ajusta App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DummyProvider } from './contexts/DummyContext';
-import { Dummy } from 'pages/Dummy';
+import { Dummy } from './pages/Dummy';
 
 const App = () => (
   <Router>
-    <Routes>
-      <DummyProvider>
-        <Route path="/dummy" element={<Dummy/>} />
-      </DummyProvider>
-    </Routes>
+    <DummyProvider>
+      <Routes>
+        <Route path="/dummy" element={<Dummy />} />
+      </Routes>
+    </DummyProvider>
   </Router>
 );
 
